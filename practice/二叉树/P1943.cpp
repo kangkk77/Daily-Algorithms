16a346395be9cde3ff64c7a0df0b2d5e28827bad
kangkk77 <2818541142@qq.com>
2026-04-01 20:09:02 +0800

    整理目录结构：将 CF/、训练赛/、search/、mati/ 归档到 contests/ 和 practice/，清理 Templates 冗余文件，删除 test.dSYM

---
+++ practice/二叉树/P1943.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve() {
+    int t;
+}
+
+signed main() {
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
